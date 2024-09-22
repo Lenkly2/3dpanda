3f9ae5edc0d2ea802cccb5e35572283ec241c6e7
Lenkly2 <143933790+Lenkly2@users.noreply.github.com>
2024-09-22 13:35:09 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from direct.showbase.ShowBase import ShowBase
 from world import Mapmanager
+from characte import Hero
 
 class Game(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
         self.land = Mapmanager()
         self.land.loadMap()
-        # base.camLens.setPov(90)
+        x,y = self.land.loadMap()
+        self.hero = Hero((x,y,2), self.land)
+        base.camLens.setFov(90)
 
 
 game = Game()
--- world.py
+++ world.py
@@ -2,36 +2,35 @@
 class Mapmanager():
     def __init__(self):
         self.model = "block.glb"
-        self.texture = "grass.png"
-        self.block = loader.loadModel(self.model)
-        self.block.setTexture(loader.loadTexture(self.texture))
-        self.color=[(60,200,1,1),
-                    (60,200,1,1),
-                    (60,200,1,1),
-                    (60,200,1,1)]
-        self.start_new()
-        self.add_block((0,10,0))
+        self.texture = "newgrass.png"
+        self.color = [(0.2, 0.2, 0.35, 1),
+                     (0.7, 0.1, 0.85, 1),
+                     (0.7, 0.9, 0.5, 1),
+                     (0.2, 0.6, 0.25, 1)
+                     ]
+        self.startNew()
+        self.addBlock((0,10,0))
 
-    def start_new(self):
+    def startNew(self):
         self.land = render.attachNewNode("Land")
 
-    def getColor(self,z):
-        if z < len(self.color):
-            return self.color[z]
+    def getColor(self, y):
+        if y < len(self.color):
+            return self.color[y]
         else:
             return self.color[len(self.color)-1]
 
-    def add_block(self,position):
-        self.block = loader.loadModel(self.model)
-        self.block.setTexture(loader.loadTexture(self.texture))
-        self.block.setPos(position)
-        self.color1 = self.getColor(int(position[2]))
-        self.block.setColor(self.color1)
-        self.block.reparentTo(self.land)
+    def addBlock(self, position):
+       self.block = loader.loadModel(self.model)
+       self.block.setTexture(loader.loadTexture(self.texture))
+       self.block.setPos(position)
+       self.c = self.getColor(int(position[2]))
+       self.block.setColor(self.c)
+       self.block.reparentTo(self.land)
 
     def loadMap(self):
         self.land.removeNode()
-        self.start_new()
+        self.startNew()
         with open("land.txt") as file:
             y = 0
             for line in file:
@@ -39,7 +38,7 @@ class Mapmanager():
                 line = line.split(" ")
                 for l in line:
                     for z0 in range(int(l)+1):
-                        block = self.add_block((x,y,z0))
+                        block = self.addBlock((x,y,z0))
                     x +=1
                 y += 1
-
+        return x,y
